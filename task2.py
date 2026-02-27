9258477b9300fdce1e58024fa6b4c51f31f3f5ed
qqreyn <duch.jan.8@gmail.com>
2026-02-27 15:24:23 +0100

    Refactor division function and input handling

--- task2.py
+++ task2.py
@@ -4,7 +4,6 @@ def division(x, y):
     except ZeroDivisionError:
         print("Division with 0 is impossible")
 
-
 print("Enter 2 numbers for division!")
 
 try:
@@ -14,3 +13,4 @@ except ValueError:
     print("NOT AN INTEGER!")
 
 print(num1, "/", num2, "=", division(num1, num2))
+
